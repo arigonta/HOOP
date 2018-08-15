864b36c58e4ff1bf29056689e66bc2623b33e27f
Mohammad Rahimyarza Bagagarsyah <rahimyarz@gmail.com>
2018-08-15 17:15:20 +0700

    implemen core data, implemen  segue ke start

--- Hoop/ViewController.swift
+++ Hoop/ViewController.swift
@@ -7,21 +7,62 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
+    var users:[User] = []
     @IBOutlet weak var nameTxt: UITextField!
     @IBOutlet weak var dobTxt: UITextField!
+    func showAlert(title: String, message: String, action: String) {
+        let alertController = UIAlertController(title: title, message:
+            message, preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: action, style: UIAlertActionStyle.default,handler: nil))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
     
     //Variable
     var userData = UserDefaults.standard
+    var dates:String = ""
     
     //action
     @IBAction func saveBtnPressed(_ sender: Any) {
-        userData.set(true, forKey: "ProfileCompleted")
-        userData.synchronize()
+        
         if nameTxt.text != "" && dobTxt.text != ""{
-            performSegue(withIdentifier: "profileToStart", sender: self)
+            let appDel = UIApplication.shared.delegate as! AppDelegate
+            let context = appDel.persistentContainer.viewContext
+            
+            do {
+                //validasi apakah user sudah terdaftar atau belum
+                let fetch: NSFetchRequest = User.fetchRequest()
+                fetch.predicate = NSPredicate(format: "name == %@", (nameTxt.text!))
+                users = try context.fetch(fetch)
+                
+                //apabila user sudah terdaftar
+                if users.count > 0 {
+                    showAlert(title: "Error", message: "Username has been taken", action: "Dismiss")
+                    print("Username has been taken")
+                }
+                    
+                    //apabila user belum terdaftar maka didaftarkan ke user baru
+                else {
+                    //memasukan data ke dalam model
+                    let newUser = NSEntityDescription.insertNewObject(forEntityName: "User", into: context)
+                    newUser.setValue(nameTxt.text, forKey: "name")
+                    newUser.setValue(dates, forKey: "dob")
+                    
+                    showAlert(title: "Success", message: "Saved", action: "OK")
+                    print("Saved")
+                    try context.save()
+                    performSegue(withIdentifier: "profileToStart", sender: self)
+                    userData.set(true, forKey: "ProfileCompleted")
+                    userData.synchronize()
+                }
+            } catch let error as NSError {
+                print("Could not fetch. \(error) \(error.userInfo)")
+            }
+            
         }
     }
     
@@ -51,6 +92,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         dateFormat.dateStyle = .long
         dateFormat.timeStyle = .none
         dobTxt.text = dateFormat.string(from: picker.date)
+        dateFormat.timeStyle = .short
+        dates = dateFormat.string(from: picker.date)
         self.view.endEditing(true)
     }
     
