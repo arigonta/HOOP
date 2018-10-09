8663a0fbd53039528c26408da52ddd92688a26d9
Mohammad Rahimyarza Bagagarsyah <rahimyarz@gmail.com>
2018-10-09 14:59:37 +0700

    mantapanjink

--- Hoop/ViewController/ActivityRecViewController.swift
+++ Hoop/ViewController/ActivityRecViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 
 
 class ActivityRecViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
-
+//yang bener
     var heartImage: String?
     var selectedIndex: Int?
     var act: [activity] = []
